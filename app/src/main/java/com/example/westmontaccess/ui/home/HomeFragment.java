cf3a9885e7fe44c013de3ad069a6a84054f403ab
「 」Data <rylanhughey@gmail.com>
2024-10-03 22:32:56 -0500

    Add main.

---
+++ app/src/main/java/com/example/westmontaccess/ui/home/HomeFragment.java
@@ -0,0 +1,45 @@
+package com.example.westmontaccess.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.westmontaccess.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        WebView webView = root.findViewById(R.id.webView); // Assuming you have a WebView with id 'webView' in your fragment layout
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com"); // Replace with your desired URL
+
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
